f08e55d16775a64b25024650f50c7c4ac3e2d7e7
Mason Wagoner <mgwagoner@gmail.com>
2022-08-17 17:54:37 -0400

    initial commit

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,9 +1,17 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 export const CurrentUser = createContext();
 
 function CurrentUserProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null)
+  useEffect(() => {
+    const getLoggedInUser = async () => {
+      let response = await fetch('http://localhost:500/authentication/profile')
+      let user = await response.json()
+      setCurrentUser(user)
+    }
+    getLoggedInUser()
+  }, [])
 
   return (
     <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
