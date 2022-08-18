6d045060189b344f2ecd81e8659872ecad266e4d
Mason Wagoner <mgwagoner@gmail.com>
2022-08-18 16:53:48 -0400

    using jwt's in rest-rant

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const db = require('../models');
 const bcrypt = require('bcrypt');
+const jwt = require('jwt')
 
 
 const { User } = db;
@@ -15,8 +16,34 @@ router.post('/', async (req, res) => {
       message: `Could not find a user with the provided username and password`,
     });
   } else {
-    res.json({ user: user  });
+    const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
+    res.json({ user: user, token: result.value });
   }
 });
+
+router.get('/profile', async (req, res) => {
+  try {
+    // Split the authorization header into [ "Bearer", "TOKEN" ]:
+    const [authenticationMethod, token] = req.header.authorization.split(' ')
+    // Only handle "Bearer" authorization for now
+    // (we could add other authorization strategies later):
+    if (authenticationMethod == 'Bearer') {
+      // Decode the JWT
+      const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+      // Get the logged in user's id from the payload
+      const { id } = result.value
+    
+    let user = await User.findOne({
+      where: {
+        userId: id
+      }
+    })
+    res.json(user)
+    }
+  } catch {
+    res.json(null)
+  }
+})
 
 module.exports = router;
