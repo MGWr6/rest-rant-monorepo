bc371d9f0d6752f8c3326219585477fa79edd9b5
Mason Wagoner <mgwagoner@gmail.com>
2022-08-18 19:35:41 -0400

    app complete

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -1,9 +1,15 @@
 const router = require('express').Router();
+const { iRegexp } = require('sequelize/types/lib/operators');
 const db = require('../models');
 
 const { Place, Comment, User } = db;
 
+let currentUser;
+
 router.post('/', async (req, res) => {
+  if (req.currentUser?.role !== 'admin') {
+    return res.status(403).json({ message: 'You are not allowed to add a place'})
+  }
   if (!req.body.pic) {
     req.body.pic = 'http://placekitten.com/400/400';
   }
@@ -45,6 +51,9 @@ router.get('/:placeId', async (req, res) => {
 });
 
 router.put('/:placeId', async (req, res) => {
+  if (req.currentUser?.role !== 'admin') {
+    return res.status(403).json({ message: 'You are not allowed to edit places'})
+  }
   let placeId = Number(req.params.placeId);
   if (isNaN(placeId)) {
     res.status(404).json({ message: `Invalid id "${placeId}"` });
@@ -65,6 +74,9 @@ router.put('/:placeId', async (req, res) => {
 });
 
 router.delete('/:placeId', async (req, res) => {
+  if (req.currentUser?.role !== 'admin') {
+    return res.status(403).json({ message: 'You are not allowed to delete places'})
+  }
   let placeId = Number(req.params.placeId);
   if (isNaN(placeId)) {
     res.status(404).json({ message: `Invalid id "${placeId}"` });
@@ -98,7 +110,7 @@ router.post('/:placeId/comments', async (req, res) => {
     return res.status(404).json({ message: `Could not find place with id "${placeId}"` });
   }
 
-  let currentUser;
+  
   try {
     currentUser = await User.findOne({
       where: {
